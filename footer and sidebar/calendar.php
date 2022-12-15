c38f698296db18d5a3a88755090ad8bba919e2cb
Dhatchaya <dhatchaya2000@gmail.com>
2022-12-15 18:33:19 +0530

    calander

---
+++ footer and sidebar/calendar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reciptionist Page</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+
+<div class="center-body-right">
+                <div class="cal__container">
+                    <div class="calendar__top">
+                        <span class="arrow" id="back__arrow"><</span>
+                        <div class="calendar__day">
+                            <span class="cal__month" id="cal__month"></span>
+                            <span class="cal__date" id="cal__date"></span>
+                        </div>
+                        <div class="calendar__time">
+                            <span class="cal__time" id="cal__time"></span>
+                        </div>
+                        <span class="arrow" id="next__arrow">></span>
+                    </div>
+                    <div class="calendar__bottom">
+                        <div class="cal__weekdays" id="cal__weekdays">
+                    
+                        </div>
+                        <div class="cal__days" id="cal__days"></div>
+                    </div>
+                </div>
+            </div>
+</body>
+</html>
