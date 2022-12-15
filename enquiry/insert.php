c82f997cf18f1c027296d11ecede37134529f136
Gagana Samarasekara <gaganasamarasekara8@gmail.com>
2022-12-15 10:38:59 +0530

    Teacher enquiry done

---
+++ enquiry/insert.php
@@ -0,0 +1,23 @@
+<?php
+require_once("../LoginTchr/connection.php");
+$Subject = $_POST['sub'];	
+$Category = $_POST['category'];		
+
+if(isset($_POST['submit'])){
+    if(empty($Subject)||empty($Category)){
+            header("location:enquiry.php?Empty= Please fill in all the fields");
+    }
+}
+$q = "INSERT INTO enquiry(content,type) VALUES ('$Subject','$Category')";
+
+$result_in = mysqli_query($con,$q);
+if(!$result_in){
+    header("location:enquiry.php?Fail=Failed to Insert");
+}
+else{
+    header("location:enquiry.php");
+}
+
+mysqli_close($con)
+
+?>
