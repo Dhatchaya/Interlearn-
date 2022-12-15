d95f0a87424746da3713d6e86daef35e764daa2f
Nisaf Ahamed <87429268+nisafaha@users.noreply.github.com>
2022-12-16 01:24:58 +0530

    after updated before interim

---
+++ Nisaf/leave_apply/show_leave_page/sidebar.php
@@ -0,0 +1,84 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="style_sidebar.css?v=<?php echo time(); ?>">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter&family=Montserrat:wght@500&family=Roboto&family=Roboto+Condensed&display=swap" rel="stylesheet">
+    </head>
+    <body>
+<div class="side-bar">
+                <div class="top">    
+                    <div class="aboutme">
+                        <img src="../uploads/<?=$_SESSION['pic']?>" alt="picture"/>
+                        <span class="user-name"><?php 
+                        
+                        echo $_SESSION['User'];
+                        ?></span>
+                        <div>
+                        
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+                <div class="middle">
+                    
+                        <div class="profile">
+                            <a href="#">
+                            <img src="icons/profile.png" alt="profile"></br>
+                            <span>Edit Profile</span>
+                            </a>
+                        </div>
+                    
+                   
+                        <div class="dashboard">
+                            <a href="#">
+                            <img src="icons/dashboard.png" alt="Dashboard"></br>
+                            <span>Dashboard</span>
+                            </a>
+                        </div>
+                   
+                    
+                        <div class="home">
+                         <a href="#">
+                            <img src="icons/card.png" alt="card"></br>
+                            <span>My payments</span>
+                         </a>
+                        </div>
+                    
+                    <div class="enquiry">
+                    <a href="#">       
+                            <img src="icons/enquiry.png" alt="enquiry"></br>
+                            <span>Enquiry</span>
+                       
+                    </a>
+                    </div>
+                    
+                 
+                
+                </div>
+                <div class="bottom">
+                    <a href="#">
+                        <img src="icons/Group.png" alt="logout">
+                        <?php
+                      
+
+                            if(isset($_SESSION['User'])){
+ 
+                                echo '<a href ="../Login/logout.php?logout"> Logout </a>';
+                            }
+                           
+                            ?>
+                        
+                    </a>
+                </div>
+               
+                
+            </div>
+    </body>
+</html>
