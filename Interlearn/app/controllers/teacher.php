65cc1ae692b559c8cd37661a8a0fa60659894441
Nisaf Ahamed <87429268+nisafaha@users.noreply.github.com>
2023-02-19 16:00:25 +0530

    updated quiz

--- Interlearn/app/controllers/teacher.php
+++ Interlearn/app/controllers/teacher.php
@@ -69,31 +69,33 @@ class Teacher extends Controller
         
         $this->view('teacher/profile',$data);
     }
-    public function quizz($action=null)
+    public function quizz($action=null,$id = null)
     { 
         if(!Auth::is_teacher()){
             redirect('home');
            
         }
+   
         if($action=='add'){
             $this->view('teacher/quiz-add');
 
             $quizz  = new Quizz();
             $id = $_GET['id'];
             $quizz_row = $quizz -> where(['quizz_id'=>$id], 'quizz_id');
-
+            
             foreach($quizz_row as $row){
                 $GLOBALS['total_question'] = $row->quizz_bank;
-                // echo ($GLOBALS['total_question']);
+                $GLOBALS['qid'] = $row->quizz_id;  // getting the quizz_id to qid global
+                echo ($GLOBALS['total_question']);
             }
-
-            while($GLOBALS['total_question'] >= 0) {
-                if($GLOBALS['total_question'] > 0) {
+          
+            // while($GLOBALS['total_question'] >= 0) {
+                // if($GLOBALS['total_question'] > 0) {
                     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                     $question_number = uniqid();
                     $_POST['question_number']=$question_number;
-
+                    
                     //  $id = $_GET['id'];
                     $_POST['quizz_id']=esc($id);
                     // show($_POST);die;
@@ -105,18 +107,25 @@ class Teacher extends Controller
                     // }
                     $choice = new Choices();
                     $result = $choice-> insert($_POST);
-                }
-                    exit();
                     $GLOBALS['total_question']--;
-                }
+                    echo ($GLOBALS['total_question']);
+                    // if($GLOBALS['total_question'] == 3) {
+                    //     echo "Im 3";
+                    // }
+                    }
+                    exit();
+                    // $GLOBALS['total_question']--;
 
-            }
-
+                    // header("Location:http://localhost/Interlearn/public/teacher/quizz/add?id=".$quizz_id);
+                // }
+                
+            // }
+            
         }
         if($action=='final'){
             $this->view('teacher/quizz-final');
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
+            
                 $confirm = new Confirm();
                 $result = $confirm-> insert($_POST);
                 // if($result) {
@@ -125,20 +134,37 @@ class Teacher extends Controller
             }
             exit();
         }
+        if($action=='all'){
+            $this->view('teacher/quizz_all');
 
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
+            // $quizz = new Quizz();
+            // $id = $_GET['id'];
+            
+            // $data['row'] = $quizz->first(['quizz_id'=>$id],'quizz_id');
+            // show($data['row']);
+            exit();
+        }
+        
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            
                 $quizz_id = uniqid();
                 $_POST['quizz_id'] = $quizz_id;
+
+                
                 $quizz = new Quizz();
                 // $id = $_POST['quizz_id'];
                 $result = $quizz-> insert($_POST);
 
+                $learn = new Learning();
+                $quizz_url = "http://localhost/Interlearn/public/student/quizz/add?id=".$quizz_id;
+                $_POST['url'] = $quizz_url;
+                $result = $learn->insert($_POST);
+                
             if($result) {
                 header("Location:http://localhost/Interlearn/public/teacher/quizz/add?id=".$quizz_id);
             }
         }
-
+        
         $this->view('teacher/quizz');
     }
     
---
+++ Interlearn/app/views/teacher/quizz_all.view.php
@@ -0,0 +1,12 @@
+<?php $this -> view('includes/header'); ?>
+<?php $this -> view('includes/nav'); ?>
+<div class="mid-container">
+    <div class="quizz_left">
+        <?php $this -> view('includes/sidebar_teach'); ?>
+    </div>
+    <div class="quizz_right">
+        <p>Hello I'm all</p>
+    </div>
+</div>
+
+<?php $this -> view('includes/footer'); ?>
