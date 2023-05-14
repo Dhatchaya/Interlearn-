37816ac24e0b94d953d97112fb2d19a1006aa9bf
Gagana Samarasekara <gaganasamarasekara8@gmail.com>
2023-05-14 16:08:36 +0530

    edited

--- Interlearn/app/views/staff/student_details.view.php
+++ Interlearn/app/views/staff/student_details.view.php
@@ -4,10 +4,12 @@
 <div class="main-body-div">
 <?php if(Auth::getrole() == "Receptionist"):?>
         <?php $this->view("includes/sidebar_recep");?>
-<?php else:?>
-        <?php  if(Auth::getrole() == "Manager"):?>
+<?php elseif(Auth::getrole() == "Manager"):?>
             <?php $this->view("includes/sidebar_man"); ?>
-<?php endif;?>
+        <?php elseif(Auth::getrole() == "Teacher"):?>
+            <?php $this->view("includes/sidebar_teach"); ?>
+        <?php elseif(Auth::getrole() == "Instructor"):?>
+            <?php $this->view("includes/sidebar_ins"); ?>
 <?php endif;?>
     <div class="top-to-bottom-content">
 
